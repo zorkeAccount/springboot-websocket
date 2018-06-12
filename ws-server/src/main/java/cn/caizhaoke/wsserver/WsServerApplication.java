54fd3705841bb50b510895cd2de6ae7de62466f1
zorke <caizhaoke@idwzx.com>
2018-06-12 10:31:11 +0800

    NEW:建立springboot websocket项目工程的server服务端模块工程

---
+++ ws-server/src/main/java/cn/caizhaoke/wsserver/WsServerApplication.java
@@ -0,0 +1,12 @@
+package cn.caizhaoke.wsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsServerApplication.class, args);
+	}
+}
---
+++ ws-server/src/test/java/cn/caizhaoke/wsserver/WsServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.caizhaoke.wsserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
